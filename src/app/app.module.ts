b93b7638890777b4a6dd1cfdb0ff3fbb4780cce2
jacob-short <jacobshort.stu@gmail.com>
2022-05-12 10:11:42 -0700

    initial layout with nav, auth, todo components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { TodoComponent } from './todo/todo.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    TodoComponent,
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule
